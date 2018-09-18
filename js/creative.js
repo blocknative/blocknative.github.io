7338521733827315618e65a98354e8c9d84896ff
Ryan Angilly <ryan@angilly.com>
2018-09-18 17:13:09 -0600

    update client for demo

--- js/creative.js
+++ js/creative.js
@@ -10,12 +10,6 @@
         event.preventDefault();
     });
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
-
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
         $('.navbar-toggle:visible').click();
